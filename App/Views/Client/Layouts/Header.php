d46fd00d5245702b8d4d94099ac9fc0256193380
Group 6 <nam219021@gmail.com>
2024-11-08 00:24:58 +0700

    Update Header.php

--- App/Views/Client/Layouts/Header.php
+++ App/Views/Client/Layouts/Header.php
@@ -20,7 +20,9 @@ class Header extends BaseView
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
+            <title>Dự án 1</title>
+            <link rel="icon" href="../../../../public/uploads/users/user1.jpeg"
+                type="image/x-icon" />
             <!-- Bootstrap CSS -->
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
@@ -47,13 +49,13 @@ class Header extends BaseView
                         <li class="nav-item">
                             <a class="nav-link" href="#">Giỏ hàng</a>
                         </li>
-            
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Đăng nhập</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Đăng ký</a>
-                            </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Đăng nhập</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Đăng ký</a>
+                        </li>
                     </ul>
                     <form class="form-inline my-2 my-lg-0">
                         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
